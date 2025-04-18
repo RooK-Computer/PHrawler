cf864113fc91ff53da65bd934f0d826ba5da5474
Sebastian Srb <sebastian.srb@palasthotel.de>
2025-04-18 17:02:20 +0200

    add rudimentary jump system

--- Game/main.lua
+++ Game/main.lua
@@ -7,12 +7,16 @@ local windowWidth = 640
 local windowHeight = 480
 local scale = 1
 local activateDebug = false
+local PIXELS_PER_METER = 100
 
 function love.load()
     betaStage = sti('maps/beta-stage.lua')
     love.window.setMode(windowWidth, windowHeight)
 
-    world = windfield.newWorld(0, 0)
+    world = windfield.newWorld(0, 9.81 * PIXELS_PER_METER)
+    --world:setGravity(0, 512)
+    world:addCollisionClass('Platform')
+    world:addCollisionClass('Player')
 
     players = {
         {
@@ -66,11 +70,14 @@ function love.load()
         local player = players[i]
         player.width = 64 * scale
         player.height = 64 * scale
-        player.speed = 150
+        player.speed = 2 * PIXELS_PER_METER
+        player.canJump = 0
+        player.upThreshold = 0
         player.spritesheet = love.graphics.newImage('assets/players/' .. player.id .. '.png')
         player.grid = anim8.newGrid( 64, 64, player.spritesheet:getWidth(), player.spritesheet:getHeight() )
         player.collider = world:newBSGRectangleCollider(player.x, player.y, 15, 30 , 10)
         player.collider:setFixedRotation(true)
+        player.collider:setCollisionClass('Player')
 
         player.direction = 'right'
 
@@ -84,31 +91,55 @@ function love.load()
         player.animations.run.right = anim8.newAnimation( player.grid('1-8', 3), 0.15 )
         player.animations.run.left = anim8.newAnimation( player.grid('1-8', 3), 0.15 ):flipH()
         player.anim = player.animations.idle.right
+
+
+        player.collider:setPreSolve(function(collider_1, collider_2, contact)        
+        if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Platform' then
+          local px, py = collider_1:getPosition()
+          --print('playery: ' .. py)
+          local tx, ty = collider_2:getPosition() 
+          --print('plattformY: ' .. ty)
+            if py + 10 > ty then contact:setEnabled(false) end
+        end   
+      end)
+
     end
 
-    walls = {}
+    plattforms = {}
 
-    if betaStage.layers['PlattformColliders'] then
+    if betaStage.layers['Plattforms'] then
 
-        for i, obj in pairs(betaStage.layers['PlattformColliders'].objects) do 
-            local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
-            wall:setType('static')
-            table.insert(walls, wall)
+        for i, obj in pairs(betaStage.layers['Plattforms'].objects) do 
+            local plattform = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            plattform:setType('static')
+            plattform:setCollisionClass('Platform')
+            table.insert(plattforms, plattform)
+        end
+    end
+
+    worldLimits = {}
+
+    if betaStage.layers['WorldLimits'] then
+
+        for i, obj in pairs(betaStage.layers['WorldLimits'].objects) do 
+            local limits = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+            limits:setType('static')
+            table.insert(worldLimits, limits)
         end
     end
 
 end
 
 function love.update(dt)
-    activateDebug = false
 
     for i = 1, #players do
 
-
-        local vx = 0
-        local vy = 100
-        local isRunning = false
         local player = players[i]
+        local vx, vy = player.collider:getLinearVelocity()
+        vx = 0
+        local isRunning = false
+
+        if vy == 0 then player.canJump = 0 end
 
         if love.keyboard.isDown(player.controls.right) then
             vx = player.speed
@@ -118,17 +149,15 @@ function love.update(dt)
         end
 
         if love.keyboard.isDown(player.controls.left) then
-            vx = player.speed * -1
+            vx = -player.speed
             player.direction = 'left'
             isRunning = true
             player.anim = player.animations.run.left
         end
 
         if love.keyboard.isDown(player.controls.up) then
-            vy = 100 * -1
+            --vy = -player.speed
         end    
-
-
 
 
         if not isRunning then 
@@ -137,8 +166,8 @@ function love.update(dt)
             end
 
         end
-
         player.collider:setLinearVelocity(vx, vy)
+
         
 
         player.x = player.collider:getX() - player.width/2
@@ -147,8 +176,24 @@ function love.update(dt)
 
     end
 
-    if love.keyboard.isDown(".") then activateDebug = true end
     world:update(dt)
+end
+
+function love.keypressed(key)
+   if key == "." then activateDebug = not activateDebug end
+
+    for i = 1, #players do
+        local player = players[i]
+        local vx, vy = player.collider:getLinearVelocity()
+
+        if key == player.controls.up and player.canJump < 2 then
+            print(player.canJump)
+          player.collider:setLinearVelocity(vx, -340)
+          player.canJump = player.canJump+1
+        end
+
+    end
+
 end
 
 function love.draw()
@@ -156,7 +201,10 @@ function love.draw()
     for i = 1, #players do
         players[i].anim:draw(players[i].spritesheet, players[i].x, players[i].y, nil, scale)
     end
-    if activateDebug then world:draw() end
+    if activateDebug then 
+        world:draw() 
+        love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+    end
 end
 
 
