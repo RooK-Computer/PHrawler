9cb1f62e44140a55612fadb4ec54b21f78688379
Sebastian Srb <sebastian.srb@palasthotel.de>
2025-04-20 12:55:55 +0200

    last prototyp: broken fighting
    we stop prototyping after this commit and start refactoring

--- Game/main.lua
+++ Game/main.lua
@@ -3,18 +3,22 @@ lick.reset = true -- adds live-reloading for development; should be removed on p
 local sti = require 'lib/sti'
 local anim8 = require 'lib/anim8'
 local windfield = require 'lib/windfield'
+local inspect = require('lib/inspect') -- gives us something like var_dump in php
 local windowWidth = 640
 local windowHeight = 480
 local scale = 1
 local activateDebug = false
 local PIXELS_PER_METER = 100
 
-function love.load()
+local savedAnimationDuration = 0
+local stateActive = false
+
+function love.load(arg)
+    if arg[#arg] == "-debug" then require("mobdebug").start() end
     betaStage = sti('maps/beta-stage.lua')
     love.window.setMode(windowWidth, windowHeight)
 
     world = windfield.newWorld(0, 9.81 * PIXELS_PER_METER)
-    --world:setGravity(0, 512)
     world:addCollisionClass('Platform')
     world:addCollisionClass('Player')
 
@@ -28,6 +32,7 @@ function love.load()
                 up = 'up',
                 left = 'left',
                 right = 'right',
+                fight = 'space'
             }
         },
         {
@@ -39,6 +44,7 @@ function love.load()
                 up = 'w',
                 left = 'a',
                 right = 'd',
+                fight = 'lshift'
             }
         },
         {
@@ -50,6 +56,7 @@ function love.load()
                 up = 'i',
                 left = 'j',
                 right = 'l',
+                fight = 'h'
             }
         },
         {
@@ -61,6 +68,7 @@ function love.load()
                 up = 'g',
                 left = 'v',
                 right = 'n',
+                fight = 'c'
             }
         }
     }
@@ -72,36 +80,134 @@ function love.load()
         player.height = 64 * scale
         player.speed = 2 * PIXELS_PER_METER
         player.canJump = 0
+        player.state = 'idle'
         player.upThreshold = 0
+        player.health = 5
         player.spritesheet = love.graphics.newImage('assets/players/' .. player.id .. '.png')
         player.grid = anim8.newGrid( 64, 64, player.spritesheet:getWidth(), player.spritesheet:getHeight() )
-        player.collider = world:newBSGRectangleCollider(player.x, player.y, 15, 30 , 10)
-        player.collider:setFixedRotation(true)
-        player.collider:setCollisionClass('Player')
+        player.colliders = {
+            playerCollider = world:newBSGRectangleCollider(player.x, player.y, 15, 30 , 10)
+        }
+        player.colliders.playerCollider:setFixedRotation(true)
+        player.colliders.playerCollider:setCollisionClass('Player')
+        player.colliders.playerCollider:setObject(player)
 
-        player.direction = 'right'
+        player.colliderID = 'fight'.. player.id
+
+
+        player.direction = 'left'
 
         player.animations = {
             idle = {},
             run = {},
+            fight = {}
         }
-        player.animations.idle.right = anim8.newAnimation( player.grid('1-8', 1), 0.15 )
-        player.animations.idle.left = anim8.newAnimation( player.grid('1-8', 1), 0.15 ):flipH()
 
-        player.animations.run.right = anim8.newAnimation( player.grid('1-8', 3), 0.15 )
-        player.animations.run.left = anim8.newAnimation( player.grid('1-8', 3), 0.15 ):flipH()
-        player.anim = player.animations.idle.right
+        local animationDuration = 0.15
+
+        player.animations.idle.right = anim8.newAnimation( player.grid('1-8', 1), animationDuration )
+        player.animations.idle.left = anim8.newAnimation( player.grid('1-8', 1), animationDuration ):flipH()
+
+        player.animations.run.right = anim8.newAnimation( player.grid('1-8', 3), animationDuration )
+        player.animations.run.left = anim8.newAnimation( player.grid('1-8', 3), animationDuration ):flipH()          
 
 
-        player.collider:setPreSolve(function(collider_1, collider_2, contact)        
+        player.animations.fight.right = anim8.newAnimation( player.grid('5-8', 9), animationDuration )
+        player.animations.fight.left = anim8.newAnimation( player.grid('5-8', 9), animationDuration ):flipH()
+ 
+        player.anim = player.animations.idle.right --default
+
+
+        player.updateAnimation = function(dt)
+                if player.state == 'idle' then
+                    if player.direction == 'right' then player.anim = player.animations.idle.right
+                    else player.anim = player.animations.idle.left
+                    end
+                end
+
+                if player.state == 'running' then
+                    if player.direction == 'right' then player.anim = player.animations.run.right
+                    else player.anim = player.animations.run.left
+                    end
+                end
+
+                if player.state == 'fight' then
+
+                    local fixturesID = player.colliderID
+
+                    if not stateActive then 
+                        stateActive = true
+
+                        if player.direction == 'left' then 
+                            player.anim = player.animations.fight.left 
+                                                        
+                            if player.colliders.playerCollider.fixtures[fixturesID] == nil then
+                                player.colliders.playerCollider:addShape(fixturesID, 'RectangleShape', -10, 0, 100, player.height/2)
+                                player.colliders.playerCollider.fixtures[fixturesID]:setSensor(true)
+                                player.colliders.playerCollider.fixtures[fixturesID]:setUserData('fightFixtureLeft')
+                            end
+
+                        end
+                        if player.direction == 'right' then 
+                            player.anim = player.animations.fight.right
+ 
+                            if player.colliders.playerCollider.fixtures[fixturesID] == nil then
+                                player.colliders.playerCollider:addShape(fixturesID, 'RectangleShape', 10, 0, 100, player.height/2)
+                                player.colliders.playerCollider.fixtures[fixturesID]:setSensor(true)
+                                player.colliders.playerCollider.fixtures[fixturesID]:setUserData('fightFixtureRight')
+                            end
+                        end
+                    end
+                    savedAnimationDuration = savedAnimationDuration + dt
+                    if savedAnimationDuration > animationDuration * 3 then 
+                        savedAnimationDuration = 0 
+                        player.state = 'idle' 
+                        player.colliders.playerCollider:removeShape(fixturesID)
+                        stateActive = false
+                    end
+
+
+                end
+
+                player.anim:update(dt)
+
+            end
+
+
+
+        player.colliders.playerCollider:setPreSolve(function(collider_1, collider_2, contact)        
         if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Platform' then
-          local px, py = collider_1:getPosition()
-          --print('playery: ' .. py)
-          local tx, ty = collider_2:getPosition() 
-          --print('plattformY: ' .. ty)
-            if py + 10 > ty then contact:setEnabled(false) end
-        end   
+            local px, py = collider_1:getPosition()
+            --print('plattformY: ' .. collider_1)
+            --print(inspect(collider_1:getObject()  ))
+            --print('.--------.')
+            local tx, ty = collider_2:getPosition() 
+            --print('plattformY: ' .. collider_2)
+            if py + 10 > ty then contact:setEnabled(false) return end
+        end
+
+         if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Player' then
+            local player1 = collider_1:getObject() 
+            local player2 = collider_2:getObject() 
+            if player1.colliders.playerCollider.fixtures[player1.colliderID] then
+                --local player1 = collider_1:getObject()
+                --local fixturesID = player1.colliderID
+                print('######')
+                print(inspect(player1.name .. ' fights ' .. player2.name  ))
+                print('END')
+                player1.colliders.playerCollider:removeShape(player1.colliderID)
+
+
+                player2.health = player2.health -1 
+
+            end
+        end
       end)
+
+        player.colliders.playerCollider:setPostSolve(
+            function(collider_1, collider_2, contact) 
+            end
+        )
 
     end
 
@@ -113,6 +219,9 @@ function love.load()
             local plattform = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
             plattform:setType('static')
             plattform:setCollisionClass('Platform')
+            plattform:setObject(plattform)
+
+            --plattform:setUserData({name = 'plattform_'..i})
             table.insert(plattforms, plattform)
         end
     end
@@ -135,44 +244,32 @@ function love.update(dt)
     for i = 1, #players do
 
         local player = players[i]
-        local vx, vy = player.collider:getLinearVelocity()
+        local vx, vy = player.colliders.playerCollider:getLinearVelocity()
         vx = 0
-        local isRunning = false
 
         if vy == 0 then player.canJump = 0 end
+        if vy == 0 and vx == 0 and player.state ~= 'fight' then player.state = 'idle' end
 
-        if love.keyboard.isDown(player.controls.right) then
+        if love.keyboard.isDown(player.controls.right) and player.state ~= 'fight' then
             vx = player.speed
             player.direction = 'right'
-            isRunning = true
-            player.anim = player.animations.run.right
+            player.state = 'running'
         end
 
-        if love.keyboard.isDown(player.controls.left) then
+        if love.keyboard.isDown(player.controls.left) and player.state ~= 'fight' then
             vx = -player.speed
             player.direction = 'left'
-            isRunning = true
-            player.anim = player.animations.run.left
-        end
+            player.state = 'running'
+        end 
 
-        if love.keyboard.isDown(player.controls.up) then
-            --vy = -player.speed
-        end    
-
-
-        if not isRunning then 
-            if player.direction == 'right' then player.anim = player.animations.idle.right
-            else player.anim = player.animations.idle.left
-            end
-
-        end
-        player.collider:setLinearVelocity(vx, vy)
+    
+        player.colliders.playerCollider:setLinearVelocity(vx, vy)
 
         
 
-        player.x = player.collider:getX() - player.width/2
-        player.y = player.collider:getY() - player.height/2
-        player.anim:update(dt)
+        player.x = player.colliders.playerCollider:getX() - player.width/2
+        player.y = player.colliders.playerCollider:getY() - player.height/2
+        player.updateAnimation(dt)
 
     end
 
@@ -180,16 +277,25 @@ function love.update(dt)
 end
 
 function love.keypressed(key)
-   if key == "." then activateDebug = not activateDebug end
+    if key == "." then activateDebug = not activateDebug end
+    if key == "," then         
+        for i = 1, #players do
+            print(players[i].name .. ' health: ' .. players[i].health)
+        end
+    end
 
     for i = 1, #players do
         local player = players[i]
-        local vx, vy = player.collider:getLinearVelocity()
+        local vx, vy = player.colliders.playerCollider:getLinearVelocity()
 
         if key == player.controls.up and player.canJump < 2 then
-            print(player.canJump)
-          player.collider:setLinearVelocity(vx, -340)
-          player.canJump = player.canJump+1
+            player.colliders.playerCollider:setLinearVelocity(vx, -340)
+            player.canJump = player.canJump+1
+            player.state = 'jump'
+        end
+
+        if love.keyboard.isDown(player.controls.fight) then
+            player.state = 'fight'
         end
 
     end
