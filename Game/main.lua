84075723113ae8a85ae4e3bcd8351dc587afd8b5
Enno Welbers <enno.welbers@palasthotel.de>
2025-07-06 23:11:49 +0200

    Revert "added command line argument to run in windowed mode. allows to be run in fullscreen on rook"
    This reverts commit 29d602e1b47f520d81a41df76476e87775aeb5e6.

--- Game/main.lua
+++ Game/main.lua
@@ -1,38 +1,35 @@
-require("init")
+require 'init'
 
 function love.load(arg)
-	io.stdout:setvbuf("no") -- needed for print() to work in ZeroBrane Studios Editor
-	local fullscreen = arg[1] ~= "-windowed" or false
-	if arg[#arg] == "-debug" then
-		require("mobdebug").start()
-		fullscreen = false
-	end -- needed for debugging in ZeroBrane Studios Editor
-	push:setupScreen(game.windowWidth, game.windowHeight, game.windowWidth, game.windowHeight, {
-		fullscreen = fullscreen,
-		vsync = true,
-		resizable = true,
-		highdpi = true,
-	})
+  io.stdout:setvbuf("no") -- needed for print() to work in ZeroBrane Studios Editor
+  if arg[#arg] == "-debug" then require("mobdebug").start() end -- needed for debugging in ZeroBrane Studios Editor
 
-	game.start()
+  push:setupScreen(game.windowWidth, game.windowHeight, game.windowWidth, game.windowHeight, {
+      fullscreen = false,
+      vsync = true,
+      resizable = true,
+      highdpi = true
+    })
 
-	--game.screen = StartScreen()
-	--game.screen:load()
+  game.start()
+
+  --game.screen = StartScreen()
+  --game.screen:load()
+
 end
 
 function love.update(dt)
-	game.screen:update(dt)
+  game.screen:update(dt)
 end
 
 function love.resize(w, h)
-	push:resize(w, h)
+  push:resize(w, h)
 end
 
+
 function love.draw()
-	push:start()
-	game.screen:draw()
-	if game.showFPS then
-		love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
-	end
-	push:finish()
+  push:start()
+  game.screen:draw()
+  if game.showFPS then love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10) end
+  push:finish()
 end
