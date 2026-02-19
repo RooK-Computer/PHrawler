a41e292f4dcea3c02da27868d1bf134db6628a63
Sebastian Srb <sebastian.srb@palasthotel.de>
2026-02-19 17:41:22 +0100

    same animations for every model

--- Game/src/player/config/animationConfigMale.lua
+++ Game/src/player/config/animationConfigMale.lua
@@ -1,14 +1,1 @@
 animationConfigMale = Helper.deepCloneTable(animationConfigDefault)
-animationConfigMale[Constants.FIGHT_STATE] = {
-  frames = {
-    {
-      grid = '6-8',
-      column = 12,
-    },
-        {
-      grid = '1-3',
-      column = 13,
-    }
-
-  }
-}
